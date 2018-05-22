a409a3076374c96afc5a0f2977c1883c6fbd5b72
Amir <amirhossein.gh@gmail.com>
2018-05-22 23:00:27 +0200

    Adds login info (trainspec, evalspec, runconfig)

--- classifier.py
+++ classifier.py
@@ -1,7 +1,13 @@
 import tensorflow as tf
 import utils
+from tensorflow.python import debug as tf_debug
+import shutil
 
 from train_csv_reader import TrainCSVReader
+from validation_csv_reader import ValidationCSVReader
+
+shutil.rmtree("checkpoint", ignore_errors=True)
+tf.reset_default_graph()
 
 tf.logging.set_verbosity(tf.logging.DEBUG)
 
@@ -12,9 +18,33 @@ feature_columns = [
     tf.feature_column.numeric_column(key) for key in csv_reader.feature_names()
 ]
 
-model = tf.estimator.DNNClassifier([10, 5, 5], feature_columns, n_classes=3, model_dir='checkpoint',
-                                      label_vocabulary=['setosa', 'versicolor', 'virginica'])
+runConfig = tf.estimator.RunConfig(model_dir="checkpoint", save_checkpoints_steps=100, save_summary_steps=100,
+                                   keep_checkpoint_max=5)
 
+model = tf.estimator.DNNClassifier([10, 5, 5], feature_columns, n_classes=3,
+                                   label_vocabulary=['setosa', 'versicolor', 'virginica'], config=runConfig)
 
-model.train(lambda: csv_reader.make_dataset_from_config({'batch_size': 32}))
+# hook = tf_debug.TensorBoardDebugHook("Amirs-MacBook-Pro-2.local:7000")
 
+# model.train(lambda: csv_reader.make_dataset_from_config({'batch_size': 32}), hooks=[hook])
+# model.train(lambda: csv_reader.make_dataset_from_config({'batch_size': 32}))
+
+validate_csv_reader = ValidationCSVReader(utils.abs_path_of(CONFIG_FILE))
+# model.evaluate(lambda: validate_csv_reader.make_dataset_from_config({}))
+
+train_spec = tf.estimator.TrainSpec(
+    input_fn=lambda: csv_reader.make_dataset_from_config({'batch_size': 32}), max_steps=5000)
+
+# TODO what are all these aparameters
+eval_spec = tf.estimator.EvalSpec(
+    input_fn=lambda: validate_csv_reader.make_dataset_from_config({}),
+    steps=1,  # How many batches of test data
+    start_delay_secs=0, throttle_secs=1)
+# throttle_secs=1)
+
+# start evaluating after N seconds
+# ,throttle_secs = 5)
+# evaluate every N seconds
+# , exporters = exporter)
+
+tf.estimator.train_and_evaluate(model, train_spec, eval_spec)

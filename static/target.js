9cf7c55f8213f27388752456e0a6acc9b4b0218c
Amir Ghamarian <amir@machine2learn.nl>
2018-07-31 14:37:22 +0200

    Adding multiple targets, js fix

--- static/target.js
+++ static/target.js
@@ -6,24 +6,30 @@ $(document).ready(function () {
                 "render": function (data, type, row) {
                     return data == -1 ? 'Not relevant' : data;
                 },
-                "targets": 2
+                // "targets": 2
             }
         ],
         'ordering': false,
-        'select': 'single'
+        'select': 'multi'
     });
 
     $('#submit').prop('disabled', true);
 
+    let selected = 0;
+
     $('#target tbody').on('click', 'tr', function () {
         if ($(this).hasClass('selected')) {
-            $(this).removeClass('selected');
-            $('#submit').prop('disabled', true);
+            selected -= 1;
+            if (selected === 0) {
+                $('#submit').prop('disabled', true);
+            }
         }
         else {
-            table.$('tr.selected').removeClass('selected');
             $(this).addClass('selected');
-            $('#submit').prop('disabled', false);
+            selected += 1;
+            if (selected > 0) {
+                $('#submit').prop('disabled', false);
+            }
         }
     });
 
